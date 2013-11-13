958921250249c88883d01f1f28d4ec4cc8d46b1f
TCvSon <thijs.vanson@runbox.com>
2013-11-13 15:46:38 +0100

    Added rscriptDemo.r file for syntax highlighting demo

---
+++ Rpres/rscriptDemo.r
@@ -0,0 +1,17 @@
+### This is a comment
+
+# Demo of the source editor -----------------------------------------------
+
+a <- 3
+b <- 4
+c <- a * b
+
+x <- "this is a character"
+
+squared <- function(x) {
+  x*x
+}
+
+squared(a)
+squared(b)
+squared(c)
